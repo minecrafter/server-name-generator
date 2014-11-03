80d3b6876e3ffec98c293399e17ea79e4aa1f13c
Tux <write@imaginarycode.com>
2014-11-03 17:27:49 -0500

    add new list and required functionality

--- lel.py
+++ lel.py
@@ -1,24 +1,40 @@
 import random
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, abort
 
-latin = []
-with open('list.txt') as f:
+lists = {}
+
+for list in os.listdir('lists'):
+  words = []
+  with open('lists/' + list) as f:
     for l in f:
-        line = l.strip().title()
-        if line != '' and '\\' not in line and ',' not in line:
-            latin.append(line)
+      line = l.strip().title()
+      if line != '' and '\\' not in line and ',' not in line and not line.startswith('#'):
+        words.append(line)
 
-suffix = ['MC', ' Cloud', 'Cloud', ' Network', 'Network', 'PVP', ' PVP', 'Craft', 'Block', 'Mine', 'Factions', ' Factions']
+  lists[list] = words
+
+suffix = ['MC', ' Cloud', 'Cloud', ' Network', 'Network', 'PVP', ' PVP', 'Craft', 'Block', 'Mine', 'Factions',
+          ' Factions', 'Prison', ' Prison', 'Survival', ' Survival', ' Minigames', 'Anarchy', 'Hardcore',
+          'Gaming', ' Gaming']
 
 app = Flask(__name__)
 
-@app.route('/generate')
-def generate_name():
-    return random.choice(latin) + random.choice(suffix)
+@app.route('/generate/<type>')
+def generate_name(type):
+  if type not in lists:
+    abort(404)
+  return random.choice(lists[type]) + random.choice(suffix)
+
+@app.route('/<type>')
+def list(type):
+  if type not in lists:
+    abort(404)
+  return render_template('index.html', type=type)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html', type="latin-derived")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
